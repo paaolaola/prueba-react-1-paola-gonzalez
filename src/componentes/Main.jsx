10a15f719c0246f19891ea4d4ca4ddd3e96ad256
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2023-10-24 00:11:30 -0300

    second commit

--- src/componentes/Main.jsx
+++ src/componentes/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
                 <p>
                     <strong>
                         Creemos que la base de la sociedad se fortalece al compartir con familia y amigos. Por eso y para que disfrutes junto a los tuyos te
-                        traemos todos los feriados de Chile.
+                        traemos todos los feriados legales de Chile.
                     </strong>
                 </p>
             </main>
